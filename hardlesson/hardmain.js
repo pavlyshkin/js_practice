1cef311105974a7783f20acfb50aea20e472dbbf
pavlyshkin <pasch.fedorow2013@yandex.ru>
2021-04-04 18:45:23 +0300

    Hard lesson complite

---
+++ hardlesson/hardmain.js
@@ -0,0 +1,10 @@
+let num = 266219;
+let arr = [2, 6, 6, 2, 1, 9];
+
+for (let i = 0; i < arr.length; i++){
+  sumN = arr[0] * arr[1] * arr[2] * arr[3] * arr[4] * arr[5]; 
+}
+console.log(sumN);
+
+sumN = sumN **3;
+console.log(String(sumN).slice(0,2));
