fd568b5b88ace031173a2e4fe1f3f5db49c34426
pavlyshkin <pasch.fedorow2013@yandex.ru>
2021-04-04 18:24:23 +0300

    lesson 2

---
+++ main.js
@@ -0,0 +1,20 @@
+let money = 100000;
+console.log(typeof money);
+let income = 'Фриланс';
+console.log(typeof income);
+let addExpenses = 'Еда, развлечение, такси, квартира';
+console.log(addExpenses.length);
+let deposit = true;
+console.log(typeof deposit);
+let mission = 1500000;
+let period = 6;
+let budgetDay = 150000;
+console.log(budgetDay / 30);
+
+
+// alert('Приветик?');
+// console.log('До свидания!');
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLocaleLowerCase());
+console.log(addExpenses.split());
